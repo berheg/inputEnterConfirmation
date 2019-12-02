5cc7051d2a7da9db2b61b167f15b9dc1facc2e7b
Girmay Hailu Berhe <48474199+berheg@users.noreply.github.com>
2019-12-02 23:46:58 +0100

    app js and css added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import BlogCard from '../src/components/Container/CommentBox/blogCard';
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <BlogCard />
+    </div>
+  );
+}
+
+export default App;
