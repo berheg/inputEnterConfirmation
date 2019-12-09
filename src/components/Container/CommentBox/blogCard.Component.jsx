e93a9e1ef127c7cb6ea94c46c762f8db70bb004f
Girmay Hailu Berhe <48474199+berheg@users.noreply.github.com>
2019-12-09 23:53:42 +0100

    component blogCard added

---
+++ src/components/Container/CommentBox/blogCard.Component.jsx
@@ -0,0 +1,58 @@
+import React, {Component} from "react";
+import DotButton from "../../DotButton/DotButton.Component";
+import DropDown from "../DropDown/DropDown.Component";
+import Input from "../../Input/Input.Component";
+import CommentList from "../CommentList.Component/CommentList.Component";
+import './blogCard.css';
+//BlogCard class component
+class BlogCard extends Component {
+  
+    state = {
+    inputValue: [],
+    isEnter: false,
+    showDropdown: false      
+  };
+  //Input event handler
+  handleInputChange=(event) =>{      
+    if(event.keyCode===13){
+      const newId=this.state.inputValue.length+1;
+      const newComment ={
+        comment:event.target.value,
+        id: newId 
+      }
+      this.setState({inputValue: this.state.inputValue.concat(newComment), isEnter: true});        
+    }
+  }
+  //button click event handler
+  onClickHandle=(event) =>{
+    if(this.state.showDropdown){
+      this.setState({showDropdown: false}); 
+    }
+    else{
+      this.setState({showDropdown: true});
+    }            
+  }        
+  render(){
+    const titleArray=['...','pin','mark as done','send to Trolle' ]
+    return(
+      <React.Fragment>
+      <div className="work-panel">
+        <div className="blog-card">
+          <div className="menu-right">                              
+            <DotButton title={'...'} onClickHandle={this.onClickHandle} />
+            {this.state.showDropdown && <DropDown titleArray={titleArray} onClickHandle={this.onClickHandle} /> }       
+          </div>
+          <Input name={"blogCardLabel"} title={"make blog as cards"} placeholder={"add comment..."}
+            type={"input"} 
+            handleInputChange={this.handleInputChange}
+          />         
+        </div>
+        {this.state.isEnter && <CommentList inputValue ={this.state.inputValue}/>}  
+      </div>      
+      </React.Fragment>
+    );
+  }
+  
+};
+
+export default BlogCard;
